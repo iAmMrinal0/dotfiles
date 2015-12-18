0a7ccaa169168f27d2fe6a9d4e7f2a1fb41a007f
Mrinal Purohit <mrinal1892005@gmail.com>
2015-12-18 12:35:22 +0530

    Modify backup.sh to copy personal .bin scripts

--- backup.sh
+++ backup.sh
@@ -1,4 +1,9 @@
 #! /bin/zsh
 
 echo "Starting..."
+echo "Copying .i3 folder"
 cp -R $HOME/.i3/ $HOME/dotfiles/
+echo ".i3 done"
+echo "Starting .bin folder"
+cp -R $HOME/.bin/ $HOME/dotfiles/.bin/
+echo ".bin done"
