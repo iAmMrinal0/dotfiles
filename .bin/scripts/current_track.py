d68198dca4597a3c58dad4b4b4285514e1e04037
Mrinal Purohit <mrinal1892005@gmail.com>
2016-06-18 03:07:06 +0530

    i3block: modified track script to handle stop.

--- .bin/scripts/current_track.py
+++ .bin/scripts/current_track.py
@@ -4,12 +4,15 @@ import subprocess
 def main():
     st = subprocess.getoutput("mpc")
     lin = st.split("\n")
-    sn_status = lin[1]
-    duration = lin[1].split(" ")
-    if "paused" in sn_status:
-        print(lin[0].split("-")[-1] + " [paused]")
-    elif "playing" in sn_status:
-        print(lin[0].split("-")[-1] + "   " + duration[4])
+    if len(lin) > 1:
+        sn_status = lin[1]
+        duration = lin[1].split(" ")
+        if "paused" in sn_status:
+            print(lin[0].split("-")[-1] + " [paused]")
+        elif "playing" in sn_status:
+            print(lin[0].split("-")[-1] + "   " + duration[4])
+        else:
+            print("stopped")
     else:
         print("stopped")
 
