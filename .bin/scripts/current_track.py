0bbfa67be217b603ee551aac0098eca2e74f43f0
Mrinal Purohit <mrinal1892005@gmail.com>
2016-11-15 10:50:42 +0530

    Fix bug in current track

--- .bin/scripts/current_track.py
+++ .bin/scripts/current_track.py
@@ -6,11 +6,11 @@ def main():
     lin = st.split("\n")
     if len(lin) > 1:
         sn_status = lin[1]
-        duration = lin[1].split(" ")
+        duration = lin[1].split()
         if "paused" in sn_status:
             print(lin[0].split("-")[-1] + " [paused]")
         elif "playing" in sn_status:
-            print(lin[0].split("-")[-1] + "   " + duration[4])
+            print(lin[0].split("-")[-1] + "   " + duration[2])
         else:
             print("stopped")
     else:
