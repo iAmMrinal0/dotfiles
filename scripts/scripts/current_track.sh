c1bc4abcecd3fd8eb5beb8780e538fbe79117064
Mrinal Purohit <github@mrinalpurohit.in>
2018-12-01 20:40:31 +0530

    [track] Use `playerctl` instead of `mpc` for Spotify control support

--- scripts/scripts/current_track.sh
+++ scripts/scripts/current_track.sh
@@ -5,8 +5,8 @@ py_manage() {
 }
 
 case $BLOCK_BUTTON in
-    3) mpc toggle | py_manage ;;  # right click, pause/unpause
-    4) mpc prev   | py_manage ;;  # scroll up, previous
-    5) mpc next   | py_manage ;;  # scroll down, next
-    *) mpc status | py_manage ;;
+    3) playerctl play-pause | py_manage ;;  # right click, pause/unpause
+    4) playerctl prev   | py_manage ;;  # scroll up, previous
+    5) playerctl next   | py_manage ;;  # scroll down, next
+    *) playerctl status | py_manage ;;
 esac
