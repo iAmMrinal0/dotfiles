c9fcb145f462feaa6741f2b36986a898703457f7
Mrinal Purohit <github@mrinalpurohit.in>
2018-12-05 11:53:44 +0530

    [lock] Pause music during system lock

--- scripts/scripts/lock.sh
+++ scripts/scripts/lock.sh
@@ -5,4 +5,5 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $lock ]] && convert /tmp/screen.png $lock -gravity center -composite -matte /tmp/screen.png
 i3lock -u -i /tmp/screen.png
 rm /tmp/screen.png
+playerctl pause
 xset dpms force off
