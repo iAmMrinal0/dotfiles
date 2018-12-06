ba115c2087b7fc5b07073ee42af6e2548d462245
Mrinal Purohit <github@mrinalpurohit.in>
2018-12-06 12:50:34 +0530

    [track] Use `spotify-now` to get current song info in i3blocks

--- scripts/scripts/current_track.py
+++ scripts/scripts/current_track.py
@@ -1,6 +1,16 @@
 import subprocess
 
 
+def text_split(text):
+    new_text = text.split()
+    new_text_len = len(new_text)
+    if new_text_len < 2:
+        return new_text[0]
+    elif new_text_len == 2:
+        return text
+    else:
+        return " ".join(new_text[0:2]) + "..."
+
 def main():
     st = subprocess.getoutput("mpc")
     lin = st.split("\n")
@@ -14,7 +24,16 @@ def main():
         else:
             print("stopped")
     else:
-        print("stopped")
+        title = subprocess.getoutput("spotify-now -i %title -p 'paused' -e stopped")
+        if title == "paused" or title == "stopped":
+            print(title)
+        elif title == "":
+            print("empty")
+        else:
+            artist = subprocess.getoutput("spotify-now -i '%artist'")
+            new_title = text_split(title)
+            new_artist = text_split(artist)
+            print(new_title + ' - ' + new_artist)
 
 
 if __name__ == "__main__":
