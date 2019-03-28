74287f7cdd1b9e8a8e07a43e521917a3e7cd78c9
Mrinal Purohit <github@mrinalpurohit.in>
2019-03-28 22:35:25 +0530

    [track] Only control songs playing in `Spotify` using `playerctl`
    This specific change is due to the fact that, at times, I have something
    playing in VLC and would have paused it and would forget about it and press the
    keybinding or Right click to play music, only to my surprise, it would start
    playing in VLC 🤦‍♂️

--- scripts/scripts/current_track.sh
+++ scripts/scripts/current_track.sh
@@ -5,8 +5,8 @@ py_manage() {
 }
 
 case $BLOCK_BUTTON in
-    3) playerctl play-pause | py_manage ;;  # right click, pause/unpause
-    4) playerctl prev   | py_manage ;;  # scroll up, previous
-    5) playerctl next   | py_manage ;;  # scroll down, next
-    *) playerctl status | py_manage ;;
+    3) playerctl play-pause --player=spotify | py_manage ;;  # right click, pause/unpause
+    4) playerctl prev       --player=spotify | py_manage ;;  # scroll up, previous
+    5) playerctl next       --player=spotify | py_manage ;;  # scroll down, next
+    *) playerctl status     --player=spotify | py_manage ;;
 esac
