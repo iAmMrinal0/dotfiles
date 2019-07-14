4285a45b3ebb4fc856081125d805ddf3b0cdc018
Mrinal Purohit <github@mrinalpurohit.in>
2019-07-14 23:10:05 +0530

    [curent_track] Show Spotify song info using `playerctl`

--- scripts/scripts/current_track.sh
+++ scripts/scripts/current_track.sh
@@ -1,12 +1,18 @@
-#! /bin/bash
+#! /usr/bin/env bash
 
 py_manage() {
-    python3 ~/scripts/current_track.py
+    format=$(playerctl --player=spotify metadata --format='{{status}}')
+    if [ "$format" = "Playing" ]
+      then
+       echo $(playerctl --player=spotify  metadata --format='{{title}} - {{album}}')
+    else
+       echo $format
+    fi
 }
 
 case $BLOCK_BUTTON in
-    3) playerctl play-pause --player=spotify | py_manage ;;  # right click, pause/unpause
-    4) playerctl prev       --player=spotify | py_manage ;;  # scroll up, previous
-    5) playerctl next       --player=spotify | py_manage ;;  # scroll down, next
-    *) playerctl status     --player=spotify | py_manage ;;
+    3) playerctl play-pause --player=spotify ;; # right click, pause/unpause
+    4) playerctl prev       --player=spotify ;; # scroll up, previous
+    5) playerctl next       --player=spotify ;; # scroll down, next
+    *) py_manage ;;
 esac
